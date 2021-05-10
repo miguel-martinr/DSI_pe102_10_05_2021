f0a4fa0bc8bd1cf47864b21b24c3c3efc0879ce6
Miguel Martín <alu0101209777@ull.edu.es>
2021-05-10 13:12:25 +0100

    done

--- src/index.ts
+++ src/index.ts
@@ -1,39 +1,73 @@
 import * as express from 'express';
-import {join} from 'path';
+import { spawn } from 'child_process';
+
+
+type CmdType = {
+  cmd: string,
+  args?: string[],
+};
 
 const app = express();
-app.use(express.static(join(__dirname, '../public')));
 
-// app.get('', (_, res) => {
-//   res.send('<h1>Hello world</h1>');
-// });
 
-app.get('/notes', (req, res) => {
+app.get('/execmd', (req, res) => {
   console.log(req.query);
 
-  if (!req.query.title) {
-    return res.send({
-      error: 'A title must be provided!',
+  if (!req.query.cmd) {
+    res.send({
+      error: 'A command must be provided!',
+    });
+  } else {
+    let args: string[] = [];
+    if (typeof req.query.args === 'string') args = req.query.args.split(/\s+/);
+  
+    const cmd = {cmd: req.query.cmd.toString(), args: args};
+    execCmd(cmd, (err, output) => {
+      if (err) {
+        res.send({
+          error: err,
+        });
+      } else {
+        res.send({
+          output: output,
+        });
+      }
     });
   }
 
-
-  return res.send({
-    notes: [
-      {
-        title: 'Blue note',
-        body: 'This is a blue note',
-        color: 'blue',
-      },
-      {
-        title: 'Yellow note',
-        body: 'This is a yellow note',
-        color: 'yellow',
-      },
-    ],
-  });
 });
+
+
+app.get('*', (_, res) => {
+  res.send('<h1>404 Page not found :(</h1>');
+});
+
 
 app.listen(3000, () => {
   console.log('Server is up on port 3000');
 });
+
+const execCmd = (executingCmd: CmdType, cb: (error: string | undefined, output: string | undefined) => void) => {
+  const cmd = spawn(executingCmd.cmd, executingCmd.args);
+
+  let err: Error;
+  cmd.on('error', (error) => {
+    err = error;
+  });
+
+  let cmdOutput = '';
+  cmd.stdout.on('data', (chunk) => cmdOutput += chunk);
+  
+  let cmdErr = '';
+  cmd.stderr.on('data', (chunk) => cmdErr += chunk);
+
+  cmd.on('close', () => {
+    if (err) {
+      cb(`Could not execute command: ${err.message}`, undefined);
+    } else if (cmdErr) {
+      cb(`Error while executing command. ${cmdErr}`, undefined);
+    } else {
+      cb(undefined, cmdOutput);
+    }
+  });
+};
